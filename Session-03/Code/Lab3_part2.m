41d0e7896996c39d2606251a44f9662091ed7351
MOH-Shafizadegan <mh.shafizadegan@gmail.com>
2024-03-13 14:59:31 +0330

    Section 2 completed

--- Session-03/Code/Lab3_part2.m
+++ Session-03/Code/Lab3_part2.m
@@ -10,7 +10,7 @@ fs = 256;
 
 %% Section 1
 
-plot3ch(X,fs,'');
+plot3ch(X,fs,'Mixed ECG');
 
 %% SVD
 
@@ -19,7 +19,7 @@ clc;
 [U,S,V] = svd(X);
 
 %% Section 2
-clc;
+clc;    
 figure
 for i = 1:3
     plot3dv(V(:,i), S(i, i))
@@ -28,3 +28,18 @@ end
 %% Section 3
 
 plot3ch(U(:,1:3), fs, 'U');
+
+figure;
+stem(S(1:3,:))
+xlim([-1 4])
+
+%% 
+
+S_fetus = zeros(size(S));
+S_fetus(2,2) = S(2,2);
+
+X_fetus = U * S_fetus * V';
+
+%%
+
+plot3ch(X_fetus, fs, 'Fetus ECG');
