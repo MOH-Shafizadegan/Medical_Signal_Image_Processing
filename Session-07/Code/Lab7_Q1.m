3a018c1ef686f707e4f2d0e47956935a805afe01
MOH-Shafizadegan <mh.shafizadegan@gmail.com>
2024-05-08 15:21:39 +0330

    Q1 and 4 completed

--- Session-07/Code/Lab7_Q1.m
+++ Session-07/Code/Lab7_Q1.m
@@ -1,4 +1,48 @@
 clc; clear; close all;
 
 addpath('../Data/')
-%%
+
+%% Load data
+
+img = imread('../Data/S1_Q1_utils/t1.jpg');
+
+%% Viaulization
+
+
+imshow(img);
+title('S1-Q1-utils/t1')
+
+%% 1D FFT
+
+img_slice1 = img(:,:,1);
+
+img_row_128 = img_slice1(128, :);
+img_row_128_fft = fftshift(fft(img_row_128, 128));
+
+figure();
+subplot(2,1,1);
+f = linspace(-pi,pi,length(img_row_128_fft));
+plot(f,abs(img_row_128_fft), 'linewidth', 2);
+title('Magnitude');
+xlabel('frequency');
+
+subplot(2,1,2);
+plot(f,(angle(img_row_128_fft)));
+title('Phase');
+xlabel('frequency');
+
+%% FFT2
+
+img_s1_d = img_slice1;
+img_fft2 = fftshift(fft2(img_s1_d));
+
+figure;
+subplot(1,2,1)
+imshow(img_s1_d)
+title('Original Image')
+
+subplot(1,2,2)
+fft2_dB = 10*log10(abs(img_fft2));
+fft2_dB = fft2_dB/max(max(abs(fft2_dB)));
+imshow(fft2_dB)
+title('log(Magnitude)')
--- Session-07/Code/Lab7_Q4.m
+++ Session-07/Code/Lab7_Q4.m
@@ -1,4 +1,80 @@
 clc; clear; close all;
 
 addpath('../Data/')
+
+%% Load data
+
+img = imread('../Data/S1_Q4_utils/ct.jpg');
+
+%% Viaulization
+
+
+imshow(img);
+title('S1-Q4-utils/ct')
+
 %%
+
+img_slice1 = double(img(:,:,1));
+img_fft2 = fftshift(fft2(img_slice1));
+[xF,yF] = meshgrid(-127:128,-127:128);
+
+x0 = 20;
+y0 = 40;
+
+kernel = exp(-1i*2*pi.*(xF*x0+yF*y0)/256);
+shifted_fft2 = img_fft2 .* kernel;
+img_shifted = abs(ifft2(ifftshift(shifted_fft2)));
+norm_img_shifted = img_shifted / max(max(img_shifted));
+
+figure;
+subplot(1,2,1)
+imshow(img);
+title('Original Image')
+
+subplot(1,2,2)
+imshow(norm_img_shifted)
+title('Shifted Image')
+
+%%
+
+figure;
+subplot(1,2,1)
+imshow(abs(kernel))
+title('Kernel FFT2 Magnitude')
+
+subplot(1,2,2)
+imshow(angle(kernel))
+title('Kernel FFT2 Phase')
+
+
+%% Section 2
+
+img_s1_norm = img_slice1 / max(max(img_slice1));
+img_rotated = imrotate(img_s1_norm, 30);
+
+figure;
+subplot(1,2,1)
+imshow(img_s1_norm)
+title('Original Image')
+
+subplot(1,2,2)
+imshow(img_rotated)
+title('Rotated image with 30^\circ')
+
+%%
+
+figure;
+subplot(1,2,1)
+img_fft2 = fftshift(fft2(img_s1_norm));
+img_fft2_dB = 10*log10(abs(img_fft2));
+img_fft2_norm = img_fft2_dB/max(max(abs(img_fft2_dB)));
+imshow(abs(img_fft2_norm),[])
+title('Original image FFT2 magnitude')
+
+
+subplot(1,2,2)
+img_rotated_fft2 = fftshift(fft2(img_rotated));
+img_rotated_fft2_dB = 10*log10(abs(img_rotated_fft2));
+img_rotated_fft2_norm = img_rotated_fft2_dB/max(max(abs(img_rotated_fft2_dB)));
+imshow(abs(img_rotated_fft2_norm))
+title('Rotated image FFT2 magnitude')
